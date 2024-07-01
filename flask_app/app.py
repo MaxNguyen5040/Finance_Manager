22b18400e0cbc5133e3905d1bdd0bd21ef2f8e32
MaxNguyen5040 <maxnguyen3012@gmail.com>
2024-07-01 13:45:58 -0500

    Secured routes and implemented logout

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,6 +1,32 @@
 from user_manager import UserManager
+from functools import wraps
+from flask import session
 
 user_manager = UserManager('data/users.csv')
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if 'username' not in session:
+            flash('You need to be logged in to access this page.')
+            return redirect(url_for('login'))
+        return f(*args, **kwargs)
+    return decorated_function
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if 'username' not in session:
+            flash('You need to be logged in to access this page.')
+            return redirect(url_for('login'))
+        return f(*args, **kwargs)
+    return decorated_function
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    flash('Logged out successfully!')
+    return redirect(url_for('login'))
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -18,6 +44,7 @@ def login():
         username = request.form['username']
         password = request.form['password']
         if user_manager.authenticate_user(username, password):
+            session['username'] = username
             flash('Login successful!')
             return redirect(url_for('index'))
         else:
